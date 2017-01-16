c0783a738e983c0d03830a71d6bf5fa4449427a2
andyma9812 <andy.ma1@hotmail.com>
2017-01-16 15:11:43 +1100

    all text to txt file

--- main.js
+++ main.js
@@ -2,47 +2,71 @@ var Epub = require('epub'),
     config = require('./config.json'),
     htmlToText = require('html-to-text'),
     path = require('path'),
+    fs = require('fs'),
     epubfile = config.filepath,
     imagewebroot = config.imagewebroot,
     chapterwebroot = config.chapterwebroot,
     epub = new Epub(epubfile, imagewebroot, chapterwebroot);
 
 epub.on("end", function() {
-    // epub is now usable
+    // Use Epub
     var chapterId,
-        chapterText,
         chapterList = [],
-        count = 0,
-        htmlText;
+        chapterObj = {
+            chapterText: ""
+        };
 
     epub.flow.forEach(function(chapter) {
         chapterList.push(chapter.id);
         // console.log(chapter.id);
     });
-
-    chapterId = getChapterId(count, chapterList);
+    for(i = 0; i<chapterList.length; i++) {
+        chapterId = getChapterId(i, chapterList);
+        epub.getChapter(chapterId, function(err, text) {
+            convertToText(text, chapterObj);
+            // console.log(text);
+            // console.log(chapterObj.chapterText);
+            writeText(chapterObj);
+        });
+    }
+    console.log(chapterObj.chapterText);
     // console.log(epub.metadata.title);
-    epub.getChapter(chapterId, function(err, text) {
-    htmlText = text;
+    // epub.getChapter(chapterId, function(err, text) {
     // console.log(htmlText);
-    });
-    // htmlText = htmlText.replace(/\n/g, "");
-    chapterText = htmlToText.fromString(htmlText, {
-    // chapterText = htmlToText.fromFile(path.join(__dirname, 'htmltest'), {
-        wordwrap: 130
-    // }, (err, text) => {
-    //     if (err) return console.error(err);
-    //     console.log(text);
-    });
-    console.log(chapterText);
+    // writeFile(text);
+    // convertToText(text, chapterObj);
+    // console.log(chapterObj.chapterText);
+    // writeText(chapterObj);
+    // });
 });
 epub.parse();
 
 function getChapterId(count, chapterList) {
     var currentChap;
 
-    currentChap = chapterList[5];
-    console.log(currentChap);
-    count = count + 1;
+    currentChap = chapterList[count];
+    // console.log(currentChap);
+    // count = count + 1;
     return currentChap;
+}
+
+// function writeFile(htmlText) {
+//     fs.writeFile(path.join(__dirname, 'htmltest.html'), htmlText, function(err) {
+//         if (err) return console.log(err);
+//         console.log('Wrote to htmltest.html');
+//     });
+// }
+
+function convertToText(htmlText, chapterObj) {
+    chapterObj.chapterText = htmlToText.fromString(htmlText, {
+        wordwrap: 130
+    });
+    // console.log(chapterText);
+}
+
+function writeText(chapterObj) {
+    fs.appendFile(path.join(__dirname, 'rawtext.txt'), chapterObj.chapterText, function(err) {
+        if (err) return console.log(err);
+        console.log('Wrote to rawtext.txt');
+    });
 }
